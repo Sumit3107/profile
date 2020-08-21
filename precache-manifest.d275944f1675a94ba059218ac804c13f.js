20f7b28d037b532ec0e5eb6714a91feef5fc6725
sumit <roy.sumit31@gmail.com>
2020-08-21 08:53:39 +0530

    Updates

---
+++ precache-manifest.d275944f1675a94ba059218ac804c13f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c9bed1ac2e8349201f44fcbfbb17f7",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "d36da670f1bbfba21089",
+    "url": "/profile/static/css/2.41fc952c.chunk.css"
+  },
+  {
+    "revision": "daebb098946cb23fd503",
+    "url": "/profile/static/css/main.a343bb47.chunk.css"
+  },
+  {
+    "revision": "d36da670f1bbfba21089",
+    "url": "/profile/static/js/2.34d5e1a8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/profile/static/js/2.34d5e1a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daebb098946cb23fd503",
+    "url": "/profile/static/js/main.45ed9136.chunk.js"
+  },
+  {
+    "revision": "61cd46d4453886c5db69",
+    "url": "/profile/static/js/runtime-main.0dd0eaa3.js"
+  },
+  {
+    "revision": "af0734c85eb675eecc34d89ba6fa23fa",
+    "url": "/profile/static/media/bg.af0734c8.jpg"
+  },
+  {
+    "revision": "b3c09e892d8b76e5a8d3ce38e2cb85d4",
+    "url": "/profile/static/media/logo.b3c09e89.jpg"
+  },
+  {
+    "revision": "6587c34cb157df2d9a027814cefbd453",
+    "url": "/profile/static/media/sumit.6587c34c.jpg"
+  }
+]);
